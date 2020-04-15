df496ab0a6db673c2105606056c14aafeb75748c
juankarlos999 <juan_karlos_999@hotmail.com>
2020-04-15 17:28:25 -0500

    [UPDATE]:functions print mini_shell and prototype functions Header

--- prompt.c
+++ prompt.c
@@ -1,49 +1,103 @@
 #include "shell.h"
 /**
  * prompt - function that emulates the prompt in the shell
- *
+ * @program_name: program name shell
  * Return: no return
  */
-void prompt(void)
+void prompt(char *program_name)
 {
-	char *input_line = NULL, **args = NULL, *path_exist = NULL;
-	int status = 0;
-	size_t len_line = 0;
+	char *input_line = NULL, **args = NULL, *command_exist = NULL;
+	int *status = malloc(sizeof(int)), eof_aux = 1;
+	size_t len_line = 0, command_num = 0;
 	ssize_t line_read_char = 0;
 	pid_t child;
 
-	signal(SIGINT, handle_sigint);
+	*status = 0, signal(SIGINT, handle_sigint);
 	while (1)
 	{
-		write(STDOUT_FILENO, "($) ", 4);
-		line_read_char = getline(&input_line, &len_line, stdin);
-		if (line_read_char == EOF)
-			write(STDOUT_FILENO, "\n", 1), free(input_line), exit(0);
-/*		evaluate string line size, evaluate interactive, no interactive */
-		if (line_read_char > 0 && input_line[0] != 10)
+		print_prompt();
+		line_read_char = getline(&input_line, &len_line, stdin),
+					*status = stat_ctr_c(*status), command_num++;
+		if (line_checker(line_read_char, &eof_aux, &input_line, status) > 0)
 		{
-			args = split_a(input_line, line_read_char);
-			if (args == NULL)
-				failed_split(input_line);
-			path_exist = handle_path(args[0]);
-			if (path_exist != NULL)
-			{
-				child = fork();
-				if (child == -1)
-					failed_fork(input_line, args);
-				if (child == 0)
-				{
-					if (execve(path_exist, args, NULL) == -1)
-						failed_exec(input_line, path_exist, args);
-				}
-				else
-					wait(&status), free(path_exist), free(args);
-			}
+			free(input_line), input_line = NULL;
+			if (eof_aux == 0)
+				exit_prompt("($)\n", status);
 			else
-				failed_path_exist(), free(args);
+				continue;
 		}
-		free(input_line);
-		input_line = NULL;
-		len_line = 0;
+		args = split_a(input_line, line_read_char, command_num, program_name,
+				status);
+		if (args == NULL)
+		{
+			free(input_line), input_line = NULL,
+			e_exit(&eof_aux, "($)\n", status);
+			continue;
+		}
+		command_exist = handle_path(args[0], command_num, program_name, status);
+		if (command_exist == NULL)
+		{
+			free(args), free(input_line), input_line = NULL,
+			e_exit(&eof_aux, "($)\n", status);
+			continue;
+		}
+		execute_command(&child, status, input_line, command_exist, args,
+				command_num, program_name), free(input_line), input_line = NULL;
+		if (eof_aux == 0)
+			exit_prompt("($)\n", status);
 	}
+}
+
+/**
+ * line_checker - function that checks for the value of line_read_char variable
+ * @line_read_char: variable that is goint to be evaluate
+ * @eof_aux: int variable to operate
+ * @input_line: string that may need to be free
+ * @status: integer in case exit is executed
+ *
+ * Return: 1 if input_line is empty
+ */
+int line_checker(ssize_t line_read_char, int *eof_aux, char **input_line,
+		int *status)
+{
+	int stat, aux = 0;
+
+	if (line_read_char == EOF)
+	{
+		if (isatty(0))
+			_putchar('\n');
+		free(*input_line), stat = *status, free(status), exit(stat);
+	}
+	if (line_read_char < 0)
+		stat = *status, free(status), failed_getline(*input_line);
+	if (line_read_char > 0)
+	{
+
+		*eof_aux = eof_validator(*input_line);
+		if (*input_line[0] == '\n')
+			aux++;
+	}
+	if (is_empty_string(*input_line, line_read_char) == 0)
+	{
+		if (*eof_aux == 0)
+			if (is_dot(*input_line, line_read_char) == 0)
+			{
+				_putchar('\n');
+				free(*input_line);
+				stat = *status;
+				free(status);
+				exit(stat);
+			}
+		aux++;
+	}
+	return (aux);
+}
+/**
+ * print_prompt - print the prompt symbol
+ *
+ */
+void print_prompt(void)
+{
+	if (isatty(0))
+		_writ("($) ");
 }
--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef SHELL
-#define SHELL
+#ifndef _SHELL_H_
+#define _SHELL_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -26,43 +26,87 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 /* PROTOTYPES  */
-int char_validator(char *str);
+/* Functions in auxiliar_functions.c file */
+int eof_validator(char *str);
+int check_for_file(char *command);
+void execute_command(pid_t *child, int *status, char *input_line,
+	char *command_exist, char **args, size_t command_num, char *program_name);
+void e_exit(int *eof_aux, char *str, int *status);
+/* Functions in complementary_functions.c file */
+int counter_space(char *line, ssize_t lineSize, char *delim);
+int is_empty_string(char *line, ssize_t lineSize);
+int is_dot(char *line, ssize_t lineSize);
+/* Functions in env_handler.c file */
 void print_env(void);
-
-void valid_exit(char *str);
-
-void failed_split(char *line);
-void failed_getline(void);
-void failed_fork(char *line, char **args);
-void failed_path_exist(void);
-void failed_exec(char *line, char *path, char **args);
-
-char *find_path_in_env(void);
-list_t *split_path(void);
-char *handle_path(char *command);
-
+void _strenv(char *s1);
+/* Functions in exit_handler.c file */
+int valid_exit(char *str, size_t num_sequence, char *program_name,
+		int *status);
+void print_exit_error(char *arg, size_t num_line, char *program_name);
+void exit_prompt(char *str, int *status);
+char *valid_letters(char *str, char *num_str, size_t num_sequence,
+		char *program_name, int lengt, int *status);
+/* Functions in failed_cases.c file */
+void failed_getline(char *line);
+void failed_fork(char *path, char **args, int *status);
+void failed_exec(char *line, char *path, char **args,
+		size_t command_num, char *program_name);
+/* Functions in itoa.c file */
+void print_number_2_digits(int);
+void print_number_3_digits(int);
+void print_number_4_digits(int);
+void print_num_Error(int n);
+/* Functions in not_found_cases.c file */
+void failed_command_exist(char *args, size_t num, char *program_name);
+void failed_permissions(char *args, size_t command_num, char *program_name);
+void failed_command_noexist(char *args, size_t num, char *program_name);
+void failed_env_noexist(char *args, size_t num, char *program_name);
+/* Functions in path_handler.c file */
+char *find_path_in_env(char *args, size_t num, char *program_name);
+list_t *split_path(char *args, size_t num, char *program_name);
+char *handle_path(char *command, size_t command_num, char *program_name,
+	int *status);
+void find_command_in_path(list_t *aux, char **found, char *add_slash,
+			int *status);
+int is_command_found(char *found, char *command, size_t command_num,
+		char *program_name, int status);
+/* Functions in prompt.c file */
+void prompt(char *program_name);
+int line_checker(ssize_t line_read_char, int *eof_aux, char **input_line,
+		int *status);
+void print_prompt(void);
+/* Functions in signal_handler.c file */
 void handle_sigint(int sig);
-
+int stat_ctr_c(int status);
+/* Functions in split_in_array.c file */
 void free_grid(char **grid, int height);
-char **split_a(char *line, ssize_t lineSize);
+char **split_a(char *line, ssize_t lineSize, size_t num_line,
+	char *program_name, int *status);
 int grid_size(char **grid);
 void print_array(char **a, int n);
-
+void list_to_array(list_t *head, char **array, size_t size);
+/* Functions in split_in_array.c file */
 list_t *split_l(char *line, ssize_t lineSize, char *delim);
 list_t *add_list(list_t **head, char *str);
 void free_list(list_t *head);
 size_t print_list(list_t *h);
-
+size_t list_len(list_t *h);
+/* Functions in str_manipulator.c file */
 size_t _strlen(char *s);
 void clean(char *str);
 char *_strdup(char *str);
 int _strcmp(char *s1, char *s2);
-void _strenv(char *);
 char *str_concat(char *s1, char *s2);
+/* Functions in str_str.c file */
 int _strstr(char *haystack, char *needle);
 int _strlen2(char *s);
 int _atoi(char *s);
-
-void prompt(void);
+/* Functions in _write.c file */
+int writErr(char *str);
+int _writ(char *str);
+int _putchar(char c);
+int _putcharE(char c);
+/* Global variable that contains the environment variables */
 extern char **environ;
+int counter;
 #endif /* SHELL */
